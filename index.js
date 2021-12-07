9e8e62d018a7cc61501d18130b8e5fe846cd4d37
Rafter Maulana Hokiananda <rafter_maulana_27rpl@student.smktelkom-mlg.sch.id>
2021-12-07 17:10:10 +0700

    db config

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+app.listen(8000, () => {
+    console.log(`Gassssss`);
+});
